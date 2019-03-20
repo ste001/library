19c641bcd67f27174348492d09a3579d1307ff0f
ste001 <asso99@hotmail.com>
2019-03-20 17:27:23 +0100

    Add Book tabs to the Index page

--- js/library.js
+++ js/library.js
@@ -1,3 +1,14 @@
+const book1 = new Book("Informatica Teorica", "D.Mandrioli", 241, true)
+const book2 = new Book("Fisica Generale", "S.Longhi", 383, true)
+const book3 = new Book("The Promised Neverland 8", "Kaiu Shirai", 150, false)
+const book4 = new Book("Mezzi di trasmissione", "F.Morichetti", 250, true)
+const book5 = new Book("Basi di Dati", "P.Atzeni", 500, true)
+//const book6 = new Book("Internet e Reti", "D.Musumeci", 345, false)
+
+let myLibrary = [book1, book2, book3, book4, book5]
+
+render()
+
 function Book (title, author, pages, read){
     this.title = title
     this.author = author
@@ -5,7 +16,7 @@ function Book (title, author, pages, read){
     this.read = read
     
     this.info = function(){
-        if (read) {
+        if (this.read) {
             return this.title + " by " + this.author + ", " + this.pages
                     + " pages, read."
         }
@@ -14,6 +25,26 @@ function Book (title, author, pages, read){
     }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.Tolkien", 295, false)
+function addBookToLibrary() {
+    let title = prompt("Insert the title of the book")
+    let author = prompt("Insert the author of the book")
+    let pages = prompt("How many pages does the book have?")
+    let read = prompt("Did you read the book?").toLowerCase()
+    read = (read === "yes" || read === "y") ? true : false
+    // Create a new book and add it to the library
+    const book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+}
 
-console.log(theHobbit.info());
+function render(){
+    myLibrary.forEach(book => {
+        const card = document.createElement("div")
+        let readAttribute = (book.read) ? "Read" : "Not read yet"
+        card.className = "grid-item"
+        card.innerHTML = "<b>Title:</b> " + book.title + "<br><b>Author:</b> "
+                        + book.author + "<br><b>Pages:</b> " + book.pages
+                        + "<br><b>Status:</b> " + readAttribute
+        const container = document.getElementById("container")
+        container.appendChild(card)
+    })
+}
